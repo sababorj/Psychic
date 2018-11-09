0bbb575b1132e54faa2e07e32bddb459bac8106b
Saba Schock <sababorjsaba@gmail.com>
2018-11-09 00:53:08 -0800

    First Revision

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,49 @@
-// make computer guess a random letter 
-
-// general varables
+// Counter varables
 var win = 0;
 var lose = 0;
 var Guessleft = 9;
+
+// Place holder for what user will guess
 var userGuess = [];
 
-// user press the key 
+// General function which makes random letter
+function randomLetter() {
+    return ('abcdefghijklmnopqrstuvwxyz').split('')[Math.floor(Math.random() * 26) + 1]
+};
+
+// When user press any key game will run
 document.onkeyup = function (event) {
-    var compGuess = ('abcdefghijklmnopqrstuvwxyz').split('')[Math.floor(Math.random() * 26) + 1];
+    var compGuess = randomLetter();
     Guessleft--;
     userGuess.push(event.key);
     document.getElementById("guesses").innerHTML = ` ${userGuess.join()}`;
     document.getElementById("Gleft").innerHTML = ` ${Guessleft}`;
-    // break point number 1
+
+    // Loging the results of the game
     console.log(`computer: ${compGuess}, user: ${userGuess}`);
-    // winning path 
-        if (userGuess[(userGuess.length)-1] === compGuess) {
-            win++;
-            alert(`you win this round!!! both of us thought of ${compGuess}`)
-            Guessleft = 9
-            userGuess = [];
-            document.getElementById("win").innerHTML = ` ${win}`;
-            document.getElementById("guesses").innerHTML = "";
-            document.getElementById("Gleft").innerHTML = ` ${Guessleft}`;
-        } else if (userGuess[(userGuess.length)-1] !== compGuess && Guessleft === 0) {
-            lose++;
-            alert(`you lost this round!!! it's ok keep going ;)`);
-            Guessleft = 9;
-            userGuess = [];
-            document.getElementById("Gleft").innerHTML = ` ${Guessleft}`;
-            document.getElementById("guesses").innerHTML = "";
-            document.getElementById("lose").innerHTML = ` ${lose}`;
-        }
+
+    // Winning path 
+    if (userGuess[(userGuess.length) - 1] === compGuess) {
+        win++;
+        alert(`you win this round!!! both of us thought of ${compGuess}`)
+
+        // Keep the score and reset the game
+        Guessleft = 9
+        userGuess = [];
+        document.getElementById("win").innerHTML = ` ${win}`;
+        document.getElementById("guesses").innerHTML = "";
+        document.getElementById("Gleft").innerHTML = ` ${Guessleft}`;
+
+        // losing path
+    } else if (userGuess[(userGuess.length) - 1] !== compGuess && Guessleft === 0) {
+        lose++;
+        alert(`you lost this round!!! it's ok keep going ;)`);
+
+        // Keep the score and reset the game
+        Guessleft = 9;
+        userGuess = [];
+        document.getElementById("Gleft").innerHTML = ` ${Guessleft}`;
+        document.getElementById("guesses").innerHTML = "";
+        document.getElementById("lose").innerHTML = ` ${lose}`;
     }
+}
